ca0a11998d01fce689bad5716a12c7cd51a979bf
Vinoth Rajagopal <vinohari64@gmail.com>
2019-09-22 13:49:05 +0530

    this is my first change

--- ex2/my_driver.c
+++ ex2/my_driver.c
@@ -3,6 +3,10 @@
 static int my_init(void){
 	unsigned char major_num = 237;	//run:$ "cat /proc/devices". check for an unused major number and pick
 
+
+
+
+
 	my_dev = MKDEV(major_num, 0);	//arg1 => major number, arg2 => minor number
 	printk("registering char device \"my_device\"\n");
 	register_chrdev_region(my_dev,1,"my_device"); //arg2 => number of minor numbers to be used, arg3 => name of the driver visible while checking in /proc/devices.
